6ff4349aeb5f222bd1e2fe7c14817b021c2c62ba
osxgirl <osxgirl@mac.com>
2021-06-12 14:35:48 -0400

    commit tree 94dc137e6beb0099d55de9fcd9c826de1f8d10f4

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import manageTodo from './reducers/manageTodo';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-let store = createStore(manageTodo);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+export function addTodo(todo) {
+  return {
+    type: "ADD_TODO",
+    todo: todo
+  }
+}
